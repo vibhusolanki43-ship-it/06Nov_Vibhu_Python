fd22244f498544b536cded15b0a61e3c8198b353
Vibhu Solanki <vibhusolanki43@gmail.com>
2025-11-17 16:30:34 +0530

    Add files via upload

---
+++ Lab/Module-1/table.py
@@ -0,0 +1,4 @@
+table = int(input("Enter number to print its table: "))
+
+for i in range(1,11):
+    print(f"{table} * {i} = {table*i}")
---
+++ Lab/Module-1/var.py
@@ -0,0 +1,12 @@
+'''a=45
+b=56
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Multiplicacion:", a * b)
+print("Division:", a / b)'''
+
+a=45.76
+print("a")
+print(int(a)) #typecating
+
